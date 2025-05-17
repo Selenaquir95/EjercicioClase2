d89e592317e7e7afc28b959c2ab4c80fa6d8d349
Selena Quiros Serrano <squiross@ucenfotec.ac.cr>
2025-05-17 11:31:58 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Entities;
+using System.Data.Common;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Instancia del Gato
+
+        var myCat = new Cat() { Id = 1, Name= "Misingo"};
+        Console.WriteLine("El Id del gato: " + myCat.Id);
+        Console.WriteLine("El nombre del gato: " + myCat.Name);
+        Console.WriteLine("Ingrese una descripción para el gato");
+        myCat.Description = Console.ReadLine();
+    }
+}
